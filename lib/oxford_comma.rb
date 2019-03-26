bac2c7a526e04754746dbcf46ee6b5cc7fd6e2e9
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-26 10:48:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
+                                  # state your variables
+array = ["k","i","w","i"]
+name = "durian"
+                                    #define your method
 def oxford_comma(array)
-
+  array = array.join
+  return array
 end
+
+oxford_comma(array)
+
+                                    #define your method
+
+def oxford_comma(array, arr_2)
+  array_s = (array << (" and ") << arr_2).join
+  return array_s
+end
+
+                                    #enter your method with arguments from variables
+oxford_comma(array, name)
